8899a216550e2553da8c0407f192b89e6232a937
golfsomphot <golf1991da@gmail.com>
2023-11-10 10:30:15 +0700

    edit

--- modules/saleorder.js
+++ modules/saleorder.js
@@ -2,7 +2,7 @@ const connectdatabase = require('../connectdata');
 function saleorder() {
     this.getSearchsaleorder = async function (req, res) {
         var result = { code: "000", msg: "", data: null }
-        // console.log("req", req);
+        console.log("req", req);
         try {
             var sql = "SELECT so FROM so WHERE socode LIKE'%" + req.socode + "%'";
             var jsondata = await connectdatabase.getdata(sql, true, false);
